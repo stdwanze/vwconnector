7abcdbf93d847d58d4730488507838119de001ea
Stefan Eggers <stefan.eggers@ibsolution.de>
2026-06-03 11:35:06 +0200

    new sources

--- run.js
+++ run.js
@@ -1,14 +1,19 @@
-const vw = require('./vwconnectloader');
-const {consumeState, initConsumer } = require("./stateconsumer");
 const configloader = require("config");
+const TronityConnector = require("./tronityconnector");
+const fromTronityFormat = require("./fromTronityFormat");
+const { consumeState, initConsumer } = require("./stateconsumer");
 
-
-const consumerLocation = configloader.get('Hnkr');
+const consumerLocation = configloader.get("Hnkr");
 initConsumer(consumerLocation);
-const connector = vw();
-connector.login().then(()=>connector.getIdStatus("WVWZZZED7SE023911").then((status)=>
-{
-    consumeState(status);
-    console.log(status);
-}));
 
+const creds = configloader.get("Tronity");
+const connector = new TronityConnector(creds.clientId, creds.clientSecret);
+
+connector.run().then(tronityData => {
+    if (!tronityData) return;
+    const state = fromTronityFormat(tronityData);
+    consumeState(state);
+}).catch(err => {
+    console.error("Error:", err.response ? JSON.stringify(err.response.data) : err.message);
+    process.exit(1);
+});
